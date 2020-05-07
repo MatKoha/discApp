f050b158135a3c0536e9bb5e013d944536e5833c
BuluMatziel <43291490+BuluMatziel@users.noreply.github.com>
2020-05-07 19:27:15 +0300

    First commit

---
+++ app.js
@@ -0,0 +1,188 @@
+// Otetaan tarvittavat moduulit käyttöön
+// Including necessary modules
+var mongoose = require("mongoose");
+var express = require("express");
+var cors = require('cors')
+var app = express();
+
+// This is needed to deploy the app on Heroku
+var path = require('path');
+app.use(express.static(path.join(__dirname, 'myfrontend/build')));
+// Enabling cors
+app.use(cors());
+// support parsing of application/json type post data
+app.use(express.json());
+// Specifying the port
+const port = process.env.port || 5000;
+
+// Specifying the connection address and options
+var uri = "mongodb+srv://new_user:asdfghjkl@cluster0-9tdqv.mongodb.net/discgolf_bag";
+var options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+};
+
+// Connecting to the database
+mongoose.connect(process.env.MONGODB_URI || uri, options);
+
+var db = mongoose.connection;
+
+// Defining a mongoose model
+const Disc = mongoose.model(
+    "Disc",
+    {
+        kiekko: String,
+        valmistaja: String,
+        vari: String,
+        nimi: String,
+        puhnro: String,
+        pvm: String
+    },
+
+    "discs"  // This is the collection that is going to be used
+);
+
+// Handling errors / success
+db.on("error", function () {
+    console.log("Connection error!");
+});
+
+db.once("open", function () {
+    console.log("Connected to the database!");
+});
+
+// parsing support
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+// Creating routes and functionalities
+
+// Retrieving ALL discs
+app.get("/api/getall", function (req, res) {
+    Disc.find({}, null, function (err, results) {
+        // Handling errors, returning status
+        if (err) {
+            res.status(500).json("Something went wrong " + err);
+            console.log("Something went wrong " + err);
+        }
+        // success
+        else {
+            res.status(200).json(results);
+            console.log(results);
+        }
+    });
+});
+
+app.get("/api/get/:id", function (req, res) {
+    // Poimitaan id talteen ja välitetään se tietokannan poisto-operaatioon
+    // Getting the id and sending information to the database
+    var id = req.params.id;
+
+    Disc.findById(id, function (err, results) {
+        // Handling errors, returning status
+        if (err) {
+            res.status(500).json("Something went wrong " + err);
+            console.log("Something went wrong " + err);
+        }
+        // success
+        else {
+            res.status(200).json("Found disc by id: " + results);
+            console.log("Found disc " + results);
+        }
+    });
+});
+
+// Formatting the date to the format dd/mm/yyyy
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear();
+
+today = dd + '/' + mm + '/' + yyyy;
+
+// Luodaan uusi tallennettava olio
+// Creating a new object
+app.post("/api/add", function (req, res) {
+    console.log(req.body)
+    var newDisc = new Disc({
+        kiekko: req.body.kiekko,
+        valmistaja: req.body.valmistaja,
+        vari: req.body.vari,
+        nimi: req.body.nimi,
+        puhnro: req.body.puhnro,
+        pvm: today
+    });
+
+    // Tallennetaan olio tietokantaan
+    // Saving the object to the database
+    newDisc.save(function (err, results) {
+        // Handling errors
+        if (err) {
+            res.status(500).json("Something went wrong: " + err);
+            console.log("Something went wrong" + err);
+        }
+        // Sending the succesful results back
+        else {
+            res.status(200).json("Lisätty tietokantaan: \n" + results);
+            console.log("Added:" + results);
+        }
+    })
+
+});
+
+// Muokataan leffan tietoja id-numeron perusteella. Huomaa ID-arvon lukeminen
+app.put("/api/update/:id", function (req, res) {
+    // Poimitaan id talteen
+    var id = req.params.id;
+
+    Disc.findByIdAndUpdate(id, {
+        kiekko: req.body.kiekko,
+        valmistaja: req.body.valmistaja,
+        vari: req.body.vari,
+        nimi: req.body.nimi,
+        puhnro: req.body.puhnro
+    }, { new: true }, function (err, results) {
+        if (err) {
+            res.status(500).json("Järjestelmässä tapahtui virhe" + err);
+            console.log("Järjestelmässä tapahtui virhe" + err);
+        }
+        // Muuten lähetetään tietokannan tulokset selaimelle 
+        else {
+            res.status(200).json("Päivitetty: \n" + results);
+            console.log("Päivitetty: " + results);
+        }
+    })
+});
+
+// Poistetaan kiekko id:n perusteella
+// Finding a disc by ID and deleting it
+app.delete("/api/delete/:id", function (req, res) {
+    // Poimitaan id talteen ja välitetään se tietokannan poisto-operaatioon
+    // Getting the id and sending information to the database
+    var id = req.params.id;
+
+    Disc.findByIdAndDelete(id, function (err, results) {
+        // Handling errors
+        if (err) {
+            console.log("Something went wrong: " + err);
+            res.status(500).json("Something went wrong.");
+        } // Handling error when there is nothing to delete with the selected id
+        else if (results == null) {
+            res.status(200).json("There is nothing to remove.");
+            console.log("There is nothing to remove.");
+        } // Successful request
+        else {
+            console.log("Removed disc" + results);
+            res.status(200).json("Removed disc: " + results);
+        }
+    });
+});
+
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('./myfrontend/build'))
+}
+
+// Web-palvelimen luonti Expressin avulla
+app.listen(port, function () {
+    console.log("Using port " + port);
+});
---
+++ myfrontend/src/App.js
@@ -0,0 +1,467 @@
+import React from 'react';
+import './App.css';
+import Popup from "reactjs-popup";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.updateSubmitHandler = this.updateSubmitHandler.bind(this);
+    // Creating refs for uncontrolled components
+    this.kiekkoInput = React.createRef();
+    this.valmistajaInput = React.createRef();
+    this.variInput = React.createRef();
+    this.nimiInput = React.createRef();
+    this.puhnroInput = React.createRef();
+    // Setting state
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: [],
+      kiekko: '',
+      valmistaja: '',
+      vari: '',
+      nimi: '',
+      puhnro: '',
+      search: '',
+    };
+  }
+
+  // Getting all items from the database
+  componentDidMount() {
+    fetch("http://localhost:5000/api/getall")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result
+          })
+          // Printing the results to the console
+          console.log(result);
+        },
+        // Handling errors
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+          console.log(error);
+        }
+      )
+  }
+
+  // Deletes one discs by id
+  // Each Delete button has an id value set
+  // The function retrieves the id when the button is clicked and inserts the id into the url address. 
+  // Example: delete/5eb1a1763c95c0291846603d
+
+  deleteDisc = (e) => {
+    // Retrieving the ID of the disc
+    var id = e.currentTarget.getAttribute("value");
+    fetch('http://localhost:5000/api/delete/' + id, {
+      method: 'delete',
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        // Updating the table
+        fetch("http://localhost:5000/api/getall")
+          .then(res => res.json())
+          .then(
+            (result) => {
+              this.setState({
+                isLoaded: true,
+                items: result
+              })
+              // Printing the results to the console
+              console.log(result);
+            },
+            // Handling errors
+            (error) => {
+              this.setState({
+                isLoaded: true,
+                error
+              });
+              console.log(error);
+            }
+          )
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  // Finding disc by id. Currently not in use
+  // findDisc = (e) => {
+  //   var id = e.currentTarget.getAttribute("value");
+  //   fetch('http://localhost:5000/api/get/' + id, {
+  //     method: 'get',
+  //   })
+  //     .then(response => response.json())
+  //     .then(data => {
+  //       console.log(data);
+  //     })
+  //     .catch((error) => {
+  //       console.error(error);
+  //     });
+  // }
+
+  // Updating one disc by ID
+  // ID value is found when clicking "Edit" and is then inserted into the address
+  // Example: update/5eb1a1763c95c0291846603d
+  updateSubmitHandler = (e) => {
+    e.preventDefault()
+    // Retrieving the ID of the disc
+    var id = e.currentTarget.getAttribute("value");
+    console.log(this.state)
+    // Setting the values of the input fields to State
+    this.setState({
+      kiekko: this.kiekkoInput.current.value,
+      valmistaja: this.valmistajaInput.current.value,
+      vari: this.variInput.current.value,
+      nimi: this.nimiInput.current.value,
+      puhnro: this.puhnroInput.current.value
+    }, function () {
+      fetch('http://localhost:5000/api/update/' + id, {
+        method: 'put',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(this.state),
+      })
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+          window.alert(data);
+          // Clearing the state to avoid conflicts with other input fields
+          this.setState({
+            kiekko: '',
+            valmistaja: '',
+            vari: '',
+            nimi: '',
+            puhnro: ''
+          })
+          // Updating the table
+          fetch("http://localhost:5000/api/getall")
+            .then(res => res.json())
+            .then(
+              (result) => {
+                this.setState({
+                  isLoaded: true,
+                  items: result
+                })
+                // Printing the results to the console
+                console.log(result);
+              },
+              // Handling errors
+              (error) => {
+                this.setState({
+                  isLoaded: true,
+                  error
+                });
+                console.log(error);
+              }
+            )
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    });
+  }
+
+  // Updating the searchable item as the user types
+  updateSearch(event) {
+    this.setState({ search: event.target.value.substr(0, 20) });
+  }
+
+  // Adding data from input fields to State when submitting the form
+  changeHandler = (e) => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  // Adding a disc to the database when submitting the form
+  submitHandler = (e) => {
+    e.preventDefault()
+    console.log(this.state)
+    fetch('http://localhost:5000/api/add', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(this.state),
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        window.alert(data);
+        // Clearing the state to avoid conflicts with other input fields
+        this.setState({
+          kiekko: '',
+          valmistaja: '',
+          vari: '',
+          nimi: '',
+          puhnro: ''
+        })
+        // Updating the table after successful add
+        fetch("http://localhost:5000/api/getall")
+          .then(res => res.json())
+          .then(
+            (result) => {
+              this.setState({
+                isLoaded: true,
+                items: result
+              })
+
+              // Printing the results to the console
+              console.log(result);
+            },
+            // Handling errors
+            (error) => {
+              this.setState({
+                isLoaded: true,
+                error
+              });
+              console.log(error);
+            }
+          )
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  // Toggles the "add new" button
+  Toggle = (e) => [
+    this.setState({ on: !this.state.on })
+  ]
+
+
+  render() {
+    const { error, isLoaded, items, kiekko, valmistaja, vari, nimi, puhnro } = this.state;
+    let filteredItems = items.filter(
+      (item) => {
+        // Returning items based on search results, otherwise returning whole table
+        return Object.keys(item).some(key => item[key].toString().toLowerCase().search(this.state.search.toLowerCase()) !== -1);
+      }
+    );
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div className="container">
+          {/* SEARCH BAR */}
+          {/* this.state.search gets updated onChange, so the table updates instantly */}
+          <input className="searchBar" type="search"
+            value={this.state.search}
+            onChange={this.updateSearch.bind(this)}
+            placeholder="Hae..."
+          ></input>
+          <div className="search"></div>
+          <div>
+            {/* Add new - button. Form is not visible, but is toggled when clicking the button */}
+            <button className="toggle" onClick={this.Toggle}>Uusi +</button>
+            {this.state.on && (
+              <div className="container">
+                <form onSubmit={this.submitHandler}>
+                  <div className="row">
+                    <div className="col-25">
+                      <label>Kiekko</label>
+                    </div>
+                    <div className="col-75">
+                      <input
+                        type="text"
+                        name="kiekko"
+                        value={kiekko}
+                        placeholder="Kiekko"
+                        onChange={this.changeHandler}
+                      ></input>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-25">
+                      <label>Valmistaja</label>
+                    </div>
+                    <div className="col-75">
+                      <input
+                        type="text"
+                        name="valmistaja"
+                        value={valmistaja}
+                        placeholder="Valmistaja"
+                        onChange={this.changeHandler}
+                      ></input>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-25">
+                      <label>Väri</label>
+                    </div>
+                    <div className="col-75">
+                      <input
+                        type="text"
+                        name="vari"
+                        value={vari}
+                        placeholder="Väri"
+                        onChange={this.changeHandler}
+                      ></input>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-25">
+                      <label>Nimi</label>
+                    </div>
+                    <div className="col-75">
+                      <input
+                        type="text"
+                        name="nimi"
+                        value={nimi}
+                        placeholder="Nimi"
+                        onChange={this.changeHandler}
+                      ></input>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-25">
+                      <label>Puh. Nro</label>
+                    </div>
+                    <div className="col-75">
+                      <input
+                        type="text"
+                        name="puhnro"
+                        value={puhnro}
+                        placeholder="Puhelinnro"
+                        onChange={this.changeHandler}
+                      ></input>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <button className="addNew" type="submit">Lisää uusi kiekko</button>
+                    <button className="cancel" onClick={this.Toggle}>Sulje</button>
+                  </div>
+                </form>
+              </div>
+            )}
+          </div>
+          {/* Add new - form ends here */}
+          {/* Displaying items from the database */}
+          <table>
+            <thead>
+              <tr>
+                {/* Adding table headers to the table */}
+                <th>Kiekko</th>
+                <th>Valmistaja</th>
+                <th>Väri</th>
+                <th>Nimi</th>
+                <th>Puh. Nro</th>
+                <th>Lisätty</th>
+                <th></th>
+                <th></th>
+              </tr>
+              {/* Looping through the items and inserting them to the table */}
+              {filteredItems.map(item => (
+                <tr key={item._id} id={item._id}>
+                  <td data-label="Kiekko">{item.kiekko} </td>
+                  <td data-label="Valmistaja">{item.valmistaja}</td>
+                  <td data-label="Väri">{item.vari}</td>
+                  <td data-label="Nimi">{item.nimi}</td>
+                  <td data-label="Puh. Nro">{item.puhnro}</td>
+                  <td data-label="Lisätty pvm">{item.pvm}</td>
+                  {/* Edit button. It opens a poput window, so the user can edit discs */}
+                  <td data-label="Muokkaa"><Popup trigger={<span id="edit"><i className="fa fa-edit" /></span>} modal>
+                    {close => (
+                      <div className="container">
+                        <span className="close" onClick={close}>&times;</span>
+                        {/* Submitting the form calls updateSubmitHandler function */}
+                        <form value={item._id} onSubmit={this.updateSubmitHandler}>
+                          <div className="row">
+                            <div className="col-25">
+                              <label>Kiekko</label>
+                            </div>
+                            <div className="col-75">
+                              <input
+                                type="text"
+                                name="kiekko"
+                                // The input fields have default values set, so user doesn't have to write every value again
+                                defaultValue={item.kiekko}
+                                // Using ref to get form values from the DOM, instead of using event handlers
+                                // Default values caused issues, so we have to use uncontrolled components to handle forms
+                                ref={this.kiekkoInput}
+                              >
+                              </input>
+                            </div>
+                          </div>
+                          <div className="row">
+                            <div className="col-25">
+                              <label>Valmistaja</label>
+                            </div>
+                            <div className="col-75">
+                              <input
+                                type="text"
+                                name="valmistaja"
+                                defaultValue={item.valmistaja}
+                                ref={this.valmistajaInput}
+                              >
+                              </input>
+                            </div>
+                          </div>
+                          <div className="row">
+                            <div className="col-25">
+                              <label>Väri</label>
+                            </div>
+                            <div className="col-75">
+                              <input
+                                type="text"
+                                name="vari"
+                                defaultValue={item.vari}
+                                ref={this.variInput}
+                              >
+                              </input>
+                            </div>
+                          </div>
+                          <div className="row">
+                            <div className="col-25">
+                              <label>Nimi</label>
+                            </div>
+                            <div className="col-75">
+                              <input
+                                type="text"
+                                name="nimi"
+                                defaultValue={item.nimi}
+                                ref={this.nimiInput}
+                              >
+                              </input>
+                            </div>
+                          </div>
+                          <div className="row">
+                            <div className="col-25">
+                              <label>PuhNro</label>
+                            </div>
+                            <div className="col-75">
+                              <input
+                                type="text"
+                                name="puhnro"
+                                defaultValue={item.puhnro}
+                                ref={this.puhnroInput}
+                              >
+                              </input>
+                            </div>
+                          </div>
+                          <div className="row">
+                            <button className="addNew" value={item._id} type="submit">Päivitä</button>
+                          </div>
+                        </form>
+                        <button className="cancel" onClick={() => { close(); }}>Sulje Ikkuna</button>
+                      </div>
+                    )}
+                  </Popup>
+                    {/* Edit -popup window ends here */}
+                  </td>
+                  {/* Delete button */}
+                  <td data-label="Poista"><span value={item._id} onClick={(e) => { if (window.confirm('Haluatko varmasti poistaa tämän kohteen?')) this.deleteDisc(e) }} id="trash"><i className="fa fa-trash" /></span></td>
+                </tr>
+              ))}
+            </thead>
+          </table>
+        </div >
+      );
+    }
+  }
+}
+export default App;
