d4ad9a6eaf961f13b9eaccacfaf4eeb53b2e4d95
Matias Kohanevic <matiaskohanevic@gmail.com>
2022-07-03 11:51:10 +0300

    replaced logo and text

--- myfrontend/src/components/Header/index.js
+++ myfrontend/src/components/Header/index.js
@@ -5,14 +5,11 @@ import './style.scss';
 const Header = () => {
   return (
     <div className="header">
-      <img src="http://puskasoturit.com/sites/default/files/Tekstilogo_0.png" alt="logo" />
-      <div className="title">Oletko hävittänyt frisbeegolfkiekkosi Espoon radoilla?</div>
+      <div className="title">The ultimate fribApp</div>
       <div className="text">
-        Espoon radoilta löytyy huima määrä kiekkoja, joiden omistajat ovat kateissa. Etsimmekin näille kiekoille omistajia.<br />
-        Listalta löydät löydetyt kiekot, jotka ovat päätyneet Puskasotureiden löytökiekkoihin.<br />
-        Kiekot ovat noudettavissa ennaltamäärättyinä ajankohtina, jotka ilmoitetaan Puskasotureiden Facebookissa.<br />
-        Kiekkojen noutaminen on maksutonta, mutta halutessaan voi maksaa vapaaehtoisen löytöpalkkion. Löytöpalkkioiden tuotto käytetään junioritoiminnan kehittämiseen ja tukemiseen.<br />
-        Voit käyttää sivun hakutoimintoa etsiäksesi haluttua kiekkoa, nimeä tai numeroa.
+        This is a frisbeegolf (also known as <a>disc golf</a>) related full stack app. <br />
+        The database has been implemented using MongoDB. Back-end with nodeJS + express. The front-end was built with React. <br />
+        Feel free to add, edit and remove items from "myBag".
       </div>
     </div>
   )
