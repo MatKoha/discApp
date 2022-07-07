dd8f557668a678b7aaaee285ed9d48ca1ab907a1
Matias Kohanevic <matiaskohanevic@gmail.com>
2022-07-07 13:30:47 +0300

    link update

--- myfrontend/src/components/Footer/index.js
+++ myfrontend/src/components/Footer/index.js
@@ -5,7 +5,7 @@ import './style.scss';
 const Footer = () => {
   return (
     <div className="footer">
-      <div>Designed by <a href="https://github.com/BuluMatziel">Matias Kohanevic</a></div>
+      <div>Designed by <a href="https://github.com/MatKoha">Matias Kohanevic</a></div>
       <div className="rights"> © 2020 All Rights Reserved</div>
     </div>
   )
